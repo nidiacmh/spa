20bdbce61dc083be1a63eca382e8349f2ed4ef0a
nidia <nidiacmh@gmail.com>
2020-08-20 17:06:03 -0500

    pagina de heros

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {HeroesService} from '../../servicios/heroes.service';
 
 @Component({
   selector: 'app-heroe',
@@ -6,11 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class HeroeComponent implements OnInit {
+export class HeroeComponent  {
 
-  constructor() { }
+  heroe:any = {};
 
-  ngOnInit(): void {
-  }
+  constructor(private activateRoute: ActivatedRoute,
+              private _heroesService: HeroesService) {
+
+              this.activateRoute.params.subscribe(params =>{
+              this.heroe = this._heroesService.getHeroe(params['id']);
+            });
+
+            }
+
 
 }
