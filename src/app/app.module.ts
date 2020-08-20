22b30770040b2d4fd62ce35ffbab5a28cb3e3fc2
nidia <nidiacmh@gmail.com>
2020-08-20 15:40:33 -0500

    Creacion del servicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 //Rutas
 import {APP_ROUTING} from './app.routes';
 //Servicios
+import {HeroesService} from './servicios/heroes.service';
 //Componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
@@ -22,7 +23,9 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     BrowserModule,
     APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
